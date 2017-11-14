c602959daf5f1f05bfe9acb855a05206fcd412c7
Nelson Emilio <rodrigueznelsonemilio@gmail.com>
2017-11-13 23:25:12 -0300

    Footer

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -1,9 +1,6 @@
         <footer class="main-footer">
-            <div class="pull-right hidden-xs">
-                <b>Version</b> 2.4.0
-            </div>
-            <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-            reserved.
+
+            <strong>Copyright &copy; 2014-2016 </strong>
         </footer>
     </div>
     <!-- ./wrapper -->
